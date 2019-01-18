0dda1f2a32f38dcdd98626251740db86ea0551fd
Probfia <46550166+Probfia@users.noreply.github.com>
2019-01-18 19:23:48 +0800

    Update fitting.py

--- Python/fitting.py
+++ Python/fitting.py
@@ -28,7 +28,7 @@ def linear_parameters(xs,ys):
 	
 	r = (xybar - xbar*ybar)/(sqrt(xsqbar - xbar**2)*sqrt(ysqbar - ybar**2))
 	
-	return [a,b,sigma_a,sigma_b,r]
+	return [a,b,sigma_a,sigma_b,r,sigma_y]
 
 
 def linear_eq(xs,ys):
@@ -39,21 +39,44 @@ def linear_eq(xs,ys):
 	xbar = str(xbar)
 	ybar = str(ybar)
 	b_up = ''
+	b_down = ''
 	for k in range(n):
 		b_up += '(' + str(xs[k]) + '-' + xbar + ')(' + str(ys[k]) + '-' + ybar + ')'
 	for k in range(n):
 		b_down += '(' + str(xs[k]) + '-' + xbar + ')^2'
 	#eqs for a and b	
 	b_eq = '\\frac{' + b_up + '}{' + b_down + '}'
-	a_eq = ybar + '-' + str(linear_parameters[1]) +'\\times' + xbar
+	a_eq = ybar + '-' + str(linear_parameters(xs,ys)[1]) +'\\times' + xbar
 	
 	sigma_y_under = '('
 	for k in range(n):
-		sigma_y_under += '[' + str(ys[k]) + '-(' + str(linear_parameters[1]) + '+' + str(linear_parameters[0]) +')]^2 +'
+		sigma_y_under += '[' + str(ys[k]) + '-(' + str(linear_parameters(xs,ys)[1]) + '+' + str(linear_parameters(xs,ys)[0]) +')]^2 +'
 	sigma_y_under = sigma_y_under[:-1]
-	sigma_y_under += ']'
+	sigma_y_under += ')'
 	#eq for sigma_y
 	sigma_y_eq = '\\sqrt{\\frac{1}{' + str(n) + '-2}' + sigma_y_under + '}'
+	sigma_y_eq += '=' + str(linear_parameters(xs,ys)[5])[0:3]
+	
+	sigma_b_under = '['
+	for k in range(n):
+		sigma_b_under += '(' + str(xs[k])+ '-' + xbar + ')^2 +'
+	sigma_b_under = sigma_b_under[:-1] + ']'
+	
+	sigma_b_eq = '\\frac{' + str(linear_parameters(xs,ys)[5])[0:3] + '}{\\sqrt{' + str(n) + sigma_b_under + '}}'
+	sigma_b_eq += str(linear_parameters(xs,ys)[3])[0:3]
+	
+	sigma_a_eq = str(linear_parameters(xs,ys)[3])[0:3]
+	sigma_a_under = '('
+	for k in range(n):
+		sigma_a_under += str(xs[k]) + '^2 +'
+	sigma_a_under = sigma_a_under[:-1] + ')'
+	sigma_a_eq += '\\sqrt{' + sigma_a_under + '}'
+	
+	
+		
+	
+					   
+	
 	
 		
 		
