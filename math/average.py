b8186818f249a05352d8b4c66c638e619a0934c3
Benature <wbenature@163.com>
2019-10-26 00:41:19 +0800

    期中考完再来修缮代码

--- math/average.py
+++ math/average.py
@@ -1,13 +1,17 @@
-def ave(nums):
-	'''
-	计算平均值
-	'''
-	for num in nums:
-		num = float(num)
-	n = len(nums)
-	fractop = ''
-	for num in nums:
-		fractop += str(num) + '+'
-	fractop = fractop[:-1]
-	tex = '\\frac{' + fractop +'}{' + str(n) + '}  = ' + str(sum(nums)/n)[0:8]
-	return tex
+def ave(nums, name, unit="mm"):
+    '''
+    计算平均值
+    '''
+    n = len(nums)
+    fractop = ''
+    for num in nums:
+        fractop += str(num) + '+'
+    fractop = fractop[:-1]
+    tex = "测量${}$的平均值为\n".format(name)
+    tex += '$$\\bar{' + name + '} = \\frac{' + fractop + '}{' + str(n) + '} \\mathrm{' + unit + \
+        '}} = {:.3f}{{'.format(np.mean(nums)) + unit + "}$$\n"
+    tex += "标准差为$\\sigma = {:.3f}$, 即\n".format(np.std(nums))
+    tex += "$${} = {:.3f} \\pm {:.3f} \mathrm{{ {} }}$$".format(
+        name, np.mean(nums), np.std(nums), unit)
+    print(tex)
+    return tex
